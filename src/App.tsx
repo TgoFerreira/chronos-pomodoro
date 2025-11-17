0dec3377f7a9d662aa92664897cc8ad53dfe1c4d
TgoFerreira <tgo.mferreira@gmail.com>
2025-11-17 18:51:04 -0300

    Aula 23

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import './styles/theme.css';
 import './styles/global.css';
-import './components/Heading';
-import './components/Container';
+
 import { Heading } from './components/Heading';
 import { Container } from './components/Container';
+import { Logo } from './components/Logo';
 
 export default function App() {
   return (
     <>
+      <Container>
+        <Logo />
+      </Container>
       <Container>
         <Heading>HOME</Heading>
       </Container>
