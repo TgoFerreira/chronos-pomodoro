1c7c78a4f30e5af42e3e470c1526282e288b3c5b
TgoFerreira <tgo.mferreira@gmail.com>
2025-11-16 22:48:51 -0300

    Aula 21

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
 import './styles/theme.css';
 import './styles/global.css';
 import './components/Heading';
+import './components/Container';
 import { Heading } from './components/Heading';
+import { Container } from './components/Container';
 
 export default function App() {
   return (
     <>
-      <Heading />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
-        recusandae, ab voluptate sapiente temporibus, aut doloremque vitae
-        reiciendis excepturi voluptatibus, officiis possimus iste enim! Nam
-        nihil illum ipsam autem accusantium.
-      </p>
+      <Container>
+        <Heading>HOME</Heading>
+      </Container>
+      <Container>
+        <Heading>MENU</Heading>
+      </Container>
+      <Container>
+        <Heading>FORM</Heading>
+      </Container>
+      <Container>
+        <Heading>FOOTER</Heading>
+      </Container>
     </>
   );
 }
