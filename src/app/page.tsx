a128f318b73cb71dd9a08d4c847d314b5fd267c2
TgoFerreira <tgo.mferreira@gmail.com>
2025-11-21 16:49:09 -0300

    até Aula 30

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { Logo } from '@/components/Logo';
 import { Menu } from '@/components/Menu';
 import { Countdown } from '@/components/Countdown';
 import { DefaultInput } from '@/components/DefaultInput';
+import { Cycles } from '@/components/Cycles';
 
 export default function Home() {
   return (
@@ -23,13 +24,19 @@ export default function Home() {
       <Container>
         <form className='form' action=''>
           <div className='formRow'>
-            <DefaultInput type='' />
+            <DefaultInput
+              id=''
+              type='text'
+              labelText='task:'
+              placeholder='Digite algo...'
+              disabled
+            />
           </div>
           <div className='formRow'>
             <p>Próximo ciclo é de x min</p>
           </div>
           <div className='formRow'>
-            <p>Ciclos</p>
+            <Cycles />
             <p>0 0 0 0 0 0 0</p>
           </div>
         </form>
